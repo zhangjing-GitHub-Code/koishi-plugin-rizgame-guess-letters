c26bb55f0717bbbe5020b53d6bec5e14a913b143
zhangjing-GitHub-Code <zhangjing20091105@126.com>
2023-08-21 14:09:07 +0800

    sync progress

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # koishi-plugin-rizgame-guess-letters
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-rizgame-guess-letters?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-rizgame-guess-letters)
+
 音游开字母 for koishi~
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,24 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
+declare module 'koishi' {
+  interface Channel {
+      letterGameStatus: object
+    }
+}
+
+export async function apply(ctx: Context) {
   // write your plugin here
+  ctx.model.extend('channel', {
+    // 向用户表中注入字符串字段 foo
+    //letterGameStatus : 'string'
+	  //     // 你还可以配置默认值为 'bar'
+    letterGameStatus : { type: 'json', initial: { isInGame: false, gameStatus: {} } }
+  });
+  ctx.command("guess <letter:string>").action(async (_,letter)=>{
+    if(_.session.isDirect)return "请在群聊里进行游戏！";
+    let stat=await ctx.database.get('channel', {id:[_.session.channelId]},['letterGameStatus']);
+    console.log(stat);
+
+  })
 }
